d4f006a8cfa19f505fbdb087751cbad2c416be78
+ <+>
2023-02-21 22:26:26 +0300

    completed code along

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,18 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+    # We won't be using this sql code in the future
+    # def self.create_table
+    #     sql =  <<-SQL 
+    #         CREATE TABLE IF NOT EXISTS artists (
+    #             id INTEGER PRIMARY KEY,
+    #             name TEXT,
+    #             genre TEXT,
+    #             age INTEGER,
+    #             hometown TEXT
+    #         )
+    #     SQL
+      
+    #     ActiveRecord::Base.connection.execute(sql)
+         
+    # end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,14 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/artist"
+
+# This file is requiring the gems in our Gemfile and giving our program access to them.
+# Now that you know where our Rake tasks are coming from, let's use one of them to create
+# our first migration.
+
+# ENV["RACK_ENV"] here is known as an environment variable. In this case, this 
+# environment variable determines whether our code is running in a development 
+# environment, or a test environment (when running RSpec tests). RACK_ENV is a 
+# specific environment variable that is used by the sinatra-activerecord gem to 
+# determine which database to connect to: in our environment.rb file, we're specifying
+# that it should use the development database, which is configured in the database.yml file.
